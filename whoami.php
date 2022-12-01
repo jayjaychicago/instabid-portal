1cde777997387cb36ce3e77350845024d28b31da
Julien <julienpmjacquet@gmail.com>
2022-12-01 14:27:40 -0500

    Update whoami.php

--- whoami.php
+++ whoami.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require 'vendor/autoload.php';
+
 // Check if the user is logged in already
 $session = $auth0->getCredentials();
 
