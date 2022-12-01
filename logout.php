ac0ceb0c691e014c75a5d092777c3408e98a0d5e
Julien <julienpmjacquet@gmail.com>
2022-12-01 05:50:50 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,3 +1,16 @@
 <?php
+require 'vendor/autoload.php';
+
+// Load our environment variables from the .env file:
+(Dotenv\Dotenv::createImmutable(__DIR__))->load();
+
+$auth0 = new \Auth0\SDK\Auth0([
+  'domain' => $_ENV['AUTH0_DOMAIN'],
+  'clientId' => $_ENV['AUTH0_CLIENT_ID'],
+  'clientSecret' => $_ENV['AUTH0_CLIENT_SECRET'],
+  'cookieSecret' => $_ENV['AUTH0_COOKIE_SECRET'],
+    'redirectUri' => 'https://portal.instabid.io/lumino/sd.php'
+]); 
+
 header("Location: $auth0->logout());
 ?>
