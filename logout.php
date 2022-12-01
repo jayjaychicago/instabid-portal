3c6102cba22913627549bdd22a0e3fec5fa4ada0
Julien <julienpmjacquet@gmail.com>
2022-12-01 06:01:25 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -12,9 +12,9 @@ $auth0 = new \Auth0\SDK\Auth0([
   'clientId' => $_ENV['AUTH0_CLIENT_ID'],
   'clientSecret' => $_ENV['AUTH0_CLIENT_SECRET'],
   'cookieSecret' => $_ENV['AUTH0_COOKIE_SECRET'],
-    'redirectUri' => 'https://portal.instabid.io/lumino/sd.php'
+    'redirectUri' => 'https://instabid.io'
 ]); 
 
-echo $auth0->logout();
+//echo $auth0->logout();
 header($auth0->logout());
 ?>
