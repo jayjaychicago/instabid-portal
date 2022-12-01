65d7e2ae72ae8c8c7e08a463b3e08269182300f7
jayjaychicago <julienpmjacquet@gmail.com>
2022-12-01 15:51:04 -0500

    log errors

--- lumino/sd.php
+++ lumino/sd.php
@@ -3,8 +3,10 @@
 <head>
 
 <?php
-
-  require('vendor/autoload.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+  require('../vendor/autoload.php');
 
   use Auth0\SDK\Auth0;
   use Auth0\SDK\Configuration\SdkConfiguration;
