45ae3fe34c6f2dd457d66eff3a3963431b653a1a
Julien <julienpmjacquet@gmail.com>
2022-12-01 05:39:32 -0500

    Update login.php

--- login.php
+++ login.php
@@ -34,7 +34,8 @@ $auth0 = new \Auth0\SDK\Auth0([
   'domain' => $_ENV['AUTH0_DOMAIN'],
   'clientId' => $_ENV['AUTH0_CLIENT_ID'],
   'clientSecret' => $_ENV['AUTH0_CLIENT_SECRET'],
-  'cookieSecret' => $_ENV['AUTH0_COOKIE_SECRET']
+  'cookieSecret' => $_ENV['AUTH0_COOKIE_SECRET'],
+    'redirectUri' => 'http://portal.instabid.io/lumino/sd.php'
 ]); 
 
 // 👆 We're continuing from the steps above. Append this to your index.php file.
