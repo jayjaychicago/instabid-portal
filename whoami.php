e3fc74db667a49c4cdf335fbd98eb60fb6b5aa21
Julien <julienpmjacquet@gmail.com>
2022-12-01 14:37:46 -0500

    Update whoami.php

--- whoami.php
+++ whoami.php
@@ -6,7 +6,35 @@ error_reporting(E_ALL);
 
 require 'vendor/autoload.php';
 
+// Load our environment variables from the .env file:
+(Dotenv\Dotenv::createImmutable(__DIR__))->load();
+
+$env_array =getenv();
+
+/*
+echo "<h3>The list of environment variables with values are :</h3>";
+//Print all environment variable names with values
+foreach ($env_array as $key=>$value)
+{
+    echo "$key => $value <br />";
+}
+*/
+// This is all setup in AWS ELB at: https://us-east-2.console.aws.amazon.com/elasticbeanstalk/home?region=us-east-2#/environment/configuration?applicationName=Instabid&environmentId=e-e2vtbrvebm
+// In the "software" config portion of the ELB
+
+
+$auth0 = new \Auth0\SDK\Auth0([
+  'domain' => $_ENV['AUTH0_DOMAIN'],
+  'clientId' => $_ENV['AUTH0_CLIENT_ID'],
+  'clientSecret' => $_ENV['AUTH0_CLIENT_SECRET'],
+  'cookieSecret' => $_ENV['AUTH0_COOKIE_SECRET'],
+    'redirectUri' => 'https://portal.instabid.io/lumino/sd.php'
+]); 
+
+
 // Check if the user is logged in already
+echo 'hey'.$auth0;
+
 $session = $auth0->getCredentials();
 
 
