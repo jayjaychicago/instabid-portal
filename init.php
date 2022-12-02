0b003c3292f33d91765b7e5b36ac9a76c51182f7
jayjaychicago <julienpmjacquet@gmail.com>
2022-12-01 20:25:42 -0500

    log errors

--- init.php
+++ init.php
@@ -18,6 +18,17 @@ error_reporting(E_ALL);
     redirectUri: 'https://' . $_SERVER['HTTP_HOST'] . '/callback.php',
     cookieSecret: '9c04c9cccd5f7bdc4e5b7ba41cf8d53528db5704e051d89bd3e3379e0ab83c2c'
   );
+  
+  $auth0 = new Auth0([
+    'domain' => $_ENV['AUTH0_DOMAIN'],
+    'client_id' => $_ENV['AUTH0_CLIENT_ID'],
+    'client_secret' => $_ENV['AUTH0_CLIENT_SECRET'],
+    'redirect_uri' => 'https://' . $_SERVER['HTTP_HOST'] . '/callback.php',
+    'audience' => 'https://myphpnotes.auth0.com/userinfo',
+    'persist_id_token' => true,
+    'persist_access_token' => true,
+    'persist_refresh_token' => true,
+  ]);
 
 $sdk = new Auth0($configuration);
 
