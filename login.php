2531c0966dc4694effd687d7fa392a1e1277ec5f
jayjaychicago <julienpmjacquet@gmail.com>
2022-12-01 19:29:53 -0500

    log errors

--- login.php
+++ login.php
@@ -2,5 +2,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+require('vendor/autoload.php');
+
+use Auth0\SDK\Auth0;
+use Auth0\SDK\Configuration\SdkConfiguration;
 header(sprintf('Location: %s', $sdk->login()));
 ?>
