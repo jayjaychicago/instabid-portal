447c195809531431312e2383075ea407b5f0bd81
jayjaychicago <julienpmjacquet@gmail.com>
2022-12-03 04:55:29 -0500

    figuring out this apache stuff

--- index.php
+++ index.php
@@ -58,11 +58,11 @@ Route::add('/login', function() use ($auth0) {
 
 Route::add('/callback', function() use ($auth0) {
     // Have the SDK complete the authentication flow:
-    $auth0->exchange("ROUTE_URL_CALLBACK");
-
+    echo $_GET['link'];
+//    $auth0->exchange("ROUTE_URL_CALLBACK");
     // Finally, redirect our end user back to the / index route, to display their user profile:
     //echo "hello there!";
-    header("Location: " . ROUTE_URL_INDEX);
+//    header("Location: " . ROUTE_URL_INDEX);
     exit;
 });
 
