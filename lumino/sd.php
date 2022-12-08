35cd43ed5fc85c8af62a1c11d72db878e4f9c3dd
Julien <julienpmjacquet@gmail.com>
2022-12-07 23:20:49 -0500

    Update sd.php

--- lumino/sd.php
+++ lumino/sd.php
@@ -2,27 +2,7 @@
 <html>
 <head>
 
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-  require('../vendor/autoload.php');
 
-  use Auth0\SDK\Auth0;
-  use Auth0\SDK\Configuration\SdkConfiguration;
-
-  $configuration = new SdkConfiguration(
-    domain: $_ENV['AUTH0_DOMAIN'],
-    clientId: $_ENV['AUTH0_CLIENT_ID'],
-    clientSecret: $_ENV['AUTH0_CLIENT_SECRET'],
-    redirectUri: 'http://' . $_SERVER['HTTP_HOST'] . '/callback',
-    cookieSecret: '9c04c9cccd5f7bdc4e5b7ba41cf8d53528db5704e051d89bd3e3379e0ab83c2c'
-  );
-
-  $sdk = new Auth0($configuration);
-
-  require('../router.php');
-?>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-6R68SV1E7N"></script>
